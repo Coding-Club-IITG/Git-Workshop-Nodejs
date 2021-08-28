da02509c37acc9173eb523d2433fc04ae02840d7
Harsh Agrawal <harshagrawal523@gmail.com>
2021-08-28 23:54:50 +0530

    Tasks uploaded

---
+++ server/routes/router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+
+route.get('/', services.homeRoutes);
+
+route.get('/add-user', services.add_user) 
+
+
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update); 
+
+
+
+
+module.exports = route
