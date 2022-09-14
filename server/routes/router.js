8191a772ac02c7e75871f33cda544f528bcd1d85
vissshal7 <davianvishal@gmail.com>
2022-09-14 06:39:14 +0530

    Update button working perfectly fine

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const route = express.Router()
+const express = require("express");
+const route = express.Router();
 
-const services = require('../services/render');
-const controller = require('../controller/controller');
+const services = require("../services/render");
+const controller = require("../controller/controller");
 
+route.get("/", services.homeRoutes);
 
-route.get('/', services.homeRoutes);
+route.get("/add-user", services.add_user);
+route.get("/update-user", services.update_user);
 
-route.get('/add-user', services.add_user) 
+route.post("/api/users", controller.create);
+route.get("/api/users", controller.find);
+route.put("/api/users/:id", controller.update);
 
-
-route.post('/api/users', controller.create);
-route.get('/api/users', controller.find);
-route.put('/api/users/:id', controller.update); 
-
-
-
-
-module.exports = route
+module.exports = route;
